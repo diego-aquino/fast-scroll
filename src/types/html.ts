3332bafde6a16342e7a12354045e46595bb90908
Diego Aquino <diegocruzdeaquino@gmail.com>
2021-10-28 18:35:45 -0300

    feat: implement components for buttons and inputs

---
+++ src/types/html.ts
@@ -0,0 +1,4 @@
+import { ButtonHTMLAttributes, InputHTMLAttributes } from 'react';
+
+export type HTMLButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export type HTMLInputProps = InputHTMLAttributes<HTMLInputElement>;

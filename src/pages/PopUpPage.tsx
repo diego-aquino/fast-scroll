8b6554476acaedecd413b81d57cadce2de42bdd5
Diego Aquino <diegocruzdeaquino@gmail.com>
2021-10-29 16:12:28 -0300

    feat: implement popup page layout

--- src/pages/PopUpPage.tsx
+++ src/pages/PopUpPage.tsx
@@ -1,10 +1,56 @@
-import { FC } from 'react';
+import { FC, useRef } from 'react';
 
-const PopUpPage: FC = () => (
-  <div className="flex flex-col items-center justify-center p-4 text-orange-900 bg-orange-50">
-    <p>Hello from</p>
-    <p className="font-bold">Fast Scroll!</p>
-  </div>
-);
+import Button from '~/components/Button';
+import { MinusIcon, PlusIcon } from '~/components/icons';
+import Input from '~/components/Input';
+import { formatMultiplier } from '~/utils/format';
+
+const SCROLL_SPEED_MULTIPLIER_INCREMENT = 0.1;
+
+const PopUpPage: FC = () => {
+  const speedMultiplierInputRef = useRef<HTMLInputElement>(null);
+
+  function handleIncrementScrollSpeedMultitplier(increment: number) {
+    const speedMultiplierInput = speedMultiplierInputRef.current;
+    if (!speedMultiplierInput) return;
+
+    const speedMultiplier = parseFloat(speedMultiplierInput.value);
+    const newScrollSpeedMultiplier = speedMultiplier + increment;
+
+    speedMultiplierInput.value = formatMultiplier(newScrollSpeedMultiplier);
+  }
+
+  return (
+    <div className="p-4 space-y-1 bg-gray-50">
+      <div className="flex items-end justify-center space-x-1">
+        <Input
+          ref={speedMultiplierInputRef}
+          label="Scroll speed multiplier"
+          defaultValue="1x"
+          className="cursor-default"
+          readOnly
+        />
+        <Button
+          renderIcon={PlusIcon}
+          onClick={() =>
+            handleIncrementScrollSpeedMultitplier(
+              SCROLL_SPEED_MULTIPLIER_INCREMENT,
+            )
+          }
+          className="mb-1"
+        />
+        <Button
+          renderIcon={MinusIcon}
+          onClick={() =>
+            handleIncrementScrollSpeedMultitplier(
+              -SCROLL_SPEED_MULTIPLIER_INCREMENT,
+            )
+          }
+          className="mb-1"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PopUpPage;

aa16270c023beefe1d74ac97bdd65f93c4889943
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-04-13 20:46:22 -0300

    fix: disable verbosity when prettifying files

--- scripts/utils/format.ts
+++ scripts/utils/format.ts
@@ -1,5 +1,7 @@
 import { $ } from 'zx';
 
+$.verbose = false;
+
 export async function prettifyFiles(files: string[], additionalFlags: string[] = ['--loglevel=warn']): Promise<void> {
   await $`yarn prettier --write ${files} ${additionalFlags}`;
 }
